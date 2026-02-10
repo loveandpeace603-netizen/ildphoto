f6a2dbb79e051ef6197d2d50dd4e13cdba314d96
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-10 16:07:32 -0600

    Add photo entry for Nagasaki, Japan
    Added a new photo entry for Nagasaki, Japan.

--- data/photos.js
+++ data/photos.js
@@ -1,5 +1,11 @@
 const photos = [
   {
+    id: "p003",
+    file: "p003.jpg",
+    location: "Nagasaki, Japan",
+    year: "2025"
+  },
+  {
     id: "p001",
     file: "p001.jpg",
     location: "Bangkok, Thailand",
