060104da967bcb71b04f41d459f2f121cf818d79
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-10 05:59:08 +0000

    commit

--- data/photos.js
+++ data/photos.js
@@ -3,14 +3,12 @@ const photos = [
     id: "p001",
     file: "p001.jpg",
     location: "Bangkok, Thailand",
-    year: "2016",
-    description: "Purchased by dad"
+    year: "2016"
   },
   {
     id: "p002",
     file: "p002.jpg",
     location: "Bangkok, Thailand",
-    year: "2016",
-    description: "Purchased at a night market."
+    year: "2016"
   }
 ];
