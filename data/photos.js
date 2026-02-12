6e26805420d03e2104235db3e38ec952fd629920
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-12 16:36:00 -0600

    Add new photo entry for St Louis, USA

--- data/photos.js
+++ data/photos.js
@@ -167,6 +167,13 @@ const photos = [
     year: 2025,
     continent: "North America"
   },
+ {
+    id: "p027",
+    file: "p027.jpg",
+    location: "St Louis, USA",
+    year: 2025,
+    continent: "North America"
+  },
   {
     id: "p001",
     file: "p001.jpg",
