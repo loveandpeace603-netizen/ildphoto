9c6adf9e536f9ab5ac1e30e2f7a14116d00b53ca
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-12 09:34:22 -0600

    Add new photo entry for New York

--- data/photos.js
+++ data/photos.js
@@ -62,6 +62,13 @@ const photos = [
     year: 2025,
     continent: "North America"
   },
+ {
+    id: "p012",
+    file: "p012.jpg",
+    location: "Newyork, USA",
+    year: 2025,
+    continent: "North America"
+  },
   {
     id: "p001",
     file: "p001.jpg",
