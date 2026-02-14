3f1d0861e11ba71aa636ca0f5b6558c4e1f6395c
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-14 16:35:43 -0600

    Update photos.js

--- data/photos.js
+++ data/photos.js
@@ -426,6 +426,13 @@ const photos = [
     year: 2023,
     continent: "Europe"
   },
+   {
+    id: "p065",
+    file: "p065.jpg",
+    location: "Paris, France",
+    year: 2023,
+    continent: "Europe"
+  },
   {
     id: "p001",
     file: "p001.jpg",
