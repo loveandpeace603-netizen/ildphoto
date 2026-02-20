d776eda811b5a98799d855b4ec2986f6ff539afa
loveandpeace603-netizen <loveandpeace603@gmail.com>
2026-02-20 15:19:29 -0600

    Update photos.js

--- data/photos.js
+++ data/photos.js
@@ -1357,6 +1357,13 @@ const photos = [
     year: 2012,
     continent: "North America"
   },
+    {
+    id: "p221",
+    file: "p221.jpg",
+    location: "Santa Barbara, USA",
+    year: 2012,
+    continent: "North America"
+  },
    {
     id: "p220",
     file: "p220.jpg",
